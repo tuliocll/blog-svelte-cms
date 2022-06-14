c05e05de71832e31428d86e3394059801fb836c6
Tulio <tuliocll@gmail>
2022-06-14 22:27:48 +0000

    update server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('WEBSITE', 'http://127.0.0.1'),
   app: {
     keys: env.array('APP_KEYS'),
   },
