286ee2edb6718897608b234f34622f345fa0ec6c
= <=>
2022-06-26 20:35:45 -0300

    fix slug query

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -2,14 +2,17 @@ module.exports = {
   async beforeFindMany(event) {
     // Check if is searching for a one post
     if (event.params.where && event.params.where.slug) {
-      const { slug } = event.params.where;
-      const article = await strapi.query("api::post.post").findOne({
-        slug,
+      const slug = event.params.where.slug["$eq"];
+
+      const article = await strapi.db.query("api::post.post").findOne({
+        where: { slug },
       });
+
+      const views = article.views || 0;
 
       await strapi.query("api::post.post").update({
         where: { slug },
-        data: { views: article.views || 0 + 1 },
+        data: { views: views + 1 },
       });
     }
   },
