e1ad20357a89d6a48d2b9eb16cea36fbaffcae27
= <=>
2022-06-26 18:11:33 -0300

    fix mock data

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -2,12 +2,13 @@ module.exports = {
   async beforeFindMany(event) {
     // Check if is searching for a one post
     if (event.params.where && event.params.where.slug) {
+      const { slug } = event.params.where;
       const article = await strapi.query("api::post.post").findOne({
-        slug: "testeando-localhost",
+        slug,
       });
 
       await strapi.query("api::post.post").update({
-        where: { slug: "testeando-localhost" },
+        where: { slug },
         data: { views: article.views || 0 + 1 },
       });
     }
