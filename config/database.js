81f58289b574ae4b5768ef91b8a73bb2b80fee96
Tulio Calil <tuliocalil@MacBook-Air-de-Tulio.local>
2022-09-06 18:46:33 -0300

    change database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+const fs = require("fs");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        ca: fs
+          .readFileSync(`${__dirname}/../certs/ca-certificate.crt`)
+          .toString(),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
